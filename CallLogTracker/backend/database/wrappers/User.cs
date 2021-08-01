049d326882e3be88aeee4a9898ae3ae7bd608d19
Ethan Hann <36464732+ethan-hann@users.noreply.github.com>
2021-08-01 12:14:23 -0500

    Create database connector for User table

--- CallLogTracker/backend/database/wrappers/User.cs
+++ CallLogTracker/backend/database/wrappers/User.cs
@@ -6,7 +6,51 @@ using System.Threading.Tasks;
 
 namespace CallLogTracker.backend.database.wrappers
 {
-    public class User
+    public class User : IDBObject
     {
+        public int ID { get; set; }
+
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public int AssociatedCompany { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+
+        public Company GetCompany()
+        {
+            return CompanyConnector.GetCompany(AssociatedCompany);
+        }
+
+        public string Insert()
+        {
+            if (ID == 0)
+            {
+                ID = UserConnector.InsertUser(this);
+                if (ID == 0)
+                    return "An error has occured while trying to insert a user into the database.";
+                return $"User with id {{{ID}}} has been inserted.";
+            }
+            else
+            {
+                if (UserConnector.UpdateUser(this))
+                    return $"User with id {{{ID}}} has been updated.";
+                else
+                    return "An error has occured while trying to update a user in the database.";
+            }
+        }
+
+        public string Update()
+        {
+            return Insert();
+        }
+
+        public string Delete()
+        {
+            if (UserConnector.DeleteUser(this))
+                return $"User with id {{{ID}}} has been deleted.";
+            else
+                return "An error has occured while trying to delete a user in the database.";
+        }
     }
 }
---
+++ CallLogTracker/security/Hasher.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace CallLogTracker.security
+{
+    /// <summary>
+    /// This class handles passwords for the application. It is responsible for hashing passwords and authenticating passwords.
+    /// <para>Using the method, <see cref="HashPassword(string)"/> a plain-text string can be turned into a hashed password.</para>
+    /// <para>Using the method, <see cref="AuthenticatePassword(string, string)"/> a plain-text string can be compared against a hashed password for equality.</para>
+    /// <para>To control the password strength, this class has private members for the <see cref="SALT_LENGTH"/>, <see cref="ITERATION_MAX"/>, and <see cref="ITERATION_MIN"/></para>
+    /// <para>A password is hashed according to these private members and a <see cref="Random"/> object.</para>
+    /// </summary>
+    public class Hasher
+    {
+        private const int SALT_LENGTH = 64;
+        private const int ITERATION_MIN = 500;
+        private const int ITERATION_MAX = 2000;
+
+        private static readonly Random random = new Random();
+
+        /// <summary>
+        ///Generates a new Hash for the specified password. The salt and iteration count for each password is random.
+        ///</summary>
+        ///<param name="password">The plain-text password to hash</param>
+        ///<returns>A new hashed-version of the supplied password</returns>
+        public static string HashPassword(string password)
+        {
+            RNGCryptoServiceProvider saltCellar = new RNGCryptoServiceProvider();
+            byte[] salt = new byte[65];
+            saltCellar.GetBytes(salt);
+
+            Rfc2898DeriveBytes hashTool = new Rfc2898DeriveBytes(password, salt);
+            int rNum = random.Next(ITERATION_MIN, ITERATION_MAX);
+            hashTool.IterationCount = rNum;
+            byte[] hash = hashTool.GetBytes(SALT_LENGTH);
+
+            // The format that is stored in the database: IterationCount:Salt:HashedPassword
+            string databaseStoredPassword = $"{rNum}:{Convert.ToBase64String(salt)}:{Convert.ToBase64String(hash)}";
+
+            return databaseStoredPassword;
+        }
+
+        /// <summary>
+        ///Compares a non-hashed password against the hashed password from the database. Converts the non-hashed password
+        /// into a hashed password and compares using XOR against the database password. If there are no differences, the password
+        /// is the same and the user can be authenticated.
+        ///</summary>
+        ///<param name="password">The non-hashed, plain-text password to check</param>
+        ///<param name="hashedPassword">The hashed password present in the database</param>
+        ///<returns>True if the two passwords are the same; False if not.</returns>
+        public static bool AuthenticatePassword(string password, string hashedPassword)
+        {
+            try
+            {
+                string[] hashParts = hashedPassword.Split(':');
+                int iterations = Convert.ToInt32(hashParts[0]);
+                byte[] originalSalt = Convert.FromBase64String(hashParts[1]);
+                byte[] originalHash = Convert.FromBase64String(hashParts[2]);
+
+                var hashTool = new Rfc2898DeriveBytes(password, originalSalt);
+                hashTool.IterationCount = iterations;
+                byte[] testHash = hashTool.GetBytes(originalHash.Length);
+
+                // Compare the two passwords using XOR comparison
+                var differences = Convert.ToUInt32(originalHash.Length) ^ System.Convert.ToUInt32(testHash.Length);
+
+                // Iterate through the differences and ensure that each bit is equal
+                for (var position = 0; position <= Math.Min(originalHash.Length, testHash.Length) - 1; position++)
+                    differences |= Convert.ToUInt32(originalHash[position] ^ testHash[position]);
+
+                // Returns True if each bit of the password matches each bit of the hashedPassword
+                return differences == 0;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
+
